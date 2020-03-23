e901bf5d64b8611f8298e1ee8624d70cab1ccbe0
hyelim9210 <gppfla210@naver.com>
2020-03-23 16:59:37 +0900

    hotfix 브랜치에서 네 번째로 추가한 소스이다.

---
+++ dev_book/src/hotfix/HotfixTest4.java
@@ -0,0 +1,12 @@
+package hotfix;
+
+public class HotfixTest4 {
+
+	public static void main(String[] args) {
+		//hotfix 브랜치에서 저장점을 하나 더 가지기 위한  소스 추가(세 번째)
+		//hotfix 브랜치에 변동사항이 있을 때를 확인하기 위한 실습이다.
+		System.out.println("hotfix 브랜치에서 세 번째로 추가한 소스이다.");
+		
+	}
+
+}
