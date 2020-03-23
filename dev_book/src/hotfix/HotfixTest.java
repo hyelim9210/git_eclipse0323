60cad447ffda2e532a4b1a103f3a2578703f0c0a
hyelim9210 <gppfla210@naver.com>
2020-03-23 15:43:52 +0900

    hotfix 브랜치로 파일을 추가하였다. 지금은 3월 23일 3시 43분이다.

---
+++ dev_book/src/hotfix/HotfixTest.java
@@ -0,0 +1,12 @@
+package hotfix;
+
+public class HotfixTest {
+
+	public static void main(String[] args) {
+		//프로젝트명 - 우클릭 - Remote - Switch - New Branch - hotfix(작성) 해서 만들어진 것이다.
+		
+		System.out.println("hotfix 브랜치에서 추가한 소스이다.");
+		
+	}
+
+}
