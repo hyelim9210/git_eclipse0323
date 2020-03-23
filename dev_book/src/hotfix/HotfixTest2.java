d97ed588e569dc2fcb02450468a301dd1d911f93
hyelim9210 <gppfla210@naver.com>
2020-03-23 15:56:49 +0900

    hotfix 브랜치의 두 번째 소스 저장

---
+++ dev_book/src/hotfix/HotfixTest2.java
@@ -0,0 +1,12 @@
+package hotfix;
+
+public class HotfixTest2 {
+
+	public static void main(String[] args) {
+		//hotfix 브랜치에서 저장점을 하나 더 가지기 위한  소스 추가
+		//hotfix 브랜치에 변동사항이 있을 때를 확인하기 위한 실습이다.
+		System.out.println("hotfix 브랜치에서 두 번째로 추가한 소스이다.");
+		
+	}
+
+}
