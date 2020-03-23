210746b198f2797582fde5ff47945617cf24bfb8
hyelim9210 <gppfla210@naver.com>
2020-03-23 14:33:30 +0900

    git_eclipse0323 폴더에 커밋하기

---
+++ dev_book/src/com/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld 호출 성공");
+		System.out.println("한글이 잘 나오는지 확인해보자");
+	}
+
+}
